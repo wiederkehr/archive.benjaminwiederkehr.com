9ea6244badcaea6f9bc6602e9235c8bd519d981f
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2014-02-13 12:14:47 +0100

    Adaption of icons.

--- benjaminwiederkehr_2010/sidebar.php
+++ benjaminwiederkehr_2010/sidebar.php
@@ -33,12 +33,11 @@
 	</div>
 	<div class="sidebar_content">
 		<ul class="icons">
-			<li><a href="http://feeds2.feedburner.com/artillery/Portfolio" title="Follow me via RSS" class="rss_icon">RSS</a></li>
-			<li><a href="http://twitter.com/wiederkehr" title="Follow me on Twitter" class="twitter_icon">Twitter</a></li>
-			<li><a href="http://facebook.com/benjamin.wiederkehr" title="Follow me on Facebook" class="facebook_icon">Facebook</a></li>
-			<li><a href="http://wiederkehr.tumblr.com" title="Follow me on Tumblr" class="tumblr_icon">Tumblr</a></li>
-			<li><a href="https://foursquare.com/wiederkehr" title="Follow me on Foursquare" class="foursquare_icon">Foursquare</a></li>
-			<li><a href="http://www.quora.com/Benjamin-Wiederkehr" title="Follow me on Quora" class="quora_icon">Quora</a></li>
+			<li><a href="http://feeds2.feedburner.com/artillery/Portfolio" title="Follow me via RSS">RSS <i class="icon-rss"></i></a></li>
+			<li><a href="http://twitter.com/wiederkehr" title="Follow me on Twitter">Twitter <i class="icon-twitter"></i></a></li>
+			<li><a href="http://facebook.com/benjamin.wiederkehr" title="Follow me on Facebook">Facebook <i class="icon-facebook"></i></a></li>
+			<li><a href="http://wiederkehr.tumblr.com" title="Follow me on Tumblr">Tumblr <i class="icon-tumblr"></i></a></li>
+			<li><a href="https://pinterest.com/wiederkehr" title="Follow me on Pinterest">Pinterest <i class="icon-pinterest"></i></a></li>
 		</ul>
 	</div>
 	<!-- ——————————————————————————————————————————————————————————————————— WORK -->
@@ -47,8 +46,8 @@
 	</div>
 	<div class="sidebar_content">
 		<ul class="icons">
-			<li><a href="http://interactivethings.com" title="Work with Interactive Things" class="ixt_icon">Interactive Things</a></li>
-			<li><a href="http://datavisualization.ch" title="Work with Datavisualization.ch" class="datavis_icon">Datavisualization.ch</a></li>
+			<li><a href="http://interactivethings.com" title="Work with Interactive Things">Interactive Things <i class="icon-cross"></i></a></li>
+			<li><a href="http://datavisualization.ch" title="Work with Datavisualization.ch">Datavisualization.ch <i class="icon-plus"></i></a></li>
 		</ul>
 	</div>
 	<!-- ——————————————————————————————————————————————————————————————————— NOTES -->
